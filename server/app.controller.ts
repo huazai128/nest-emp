4422a074cc034d8225f06db145374ab49442720c
huazai128 <zhouweihua@joyy.com>
2022-03-11 21:49:10 +0800

    feat: 登录鉴权完成🚀

--- server/app.controller.ts
+++ server/app.controller.ts
@@ -1,22 +1,11 @@
-import { Controller, Get, Render, Req, Session, UseGuards } from '@nestjs/common';
+import { Controller, Get, Redirect, Render, Req, Res, Session, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { RouterGuard } from './guards/router.guard';
-import { Request } from 'express'
+import { Request, Response } from 'express'
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) { }
-
-    /**
-     * 请求接口； 注入res，就必须使用res,返回
-     * @param {Request} req
-     * @return {*} 
-     * @memberof AppController
-     */
-    @Get('api')
-    getApi() {
-        return { data: 121122 }
-    }
 
     /**
    * 渲染页面
@@ -37,8 +26,8 @@ export class AppController {
      */
     @Get('error')
     @Render('error')
-    getError() {
-        return { data: 12 }
+    getError(@Res() res: Response) {
+        return res.status(301).redirect('/login')
     }
 
     /**
@@ -51,7 +40,6 @@ export class AppController {
     @Get()
     @Render('index')
     getTest(@Req() req: Request) {
-        console.log(req.session)
         return { data: 12 }
     }
 
